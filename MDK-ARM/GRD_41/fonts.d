bbd23c9b10bbb6d8ef9076b60ef2dd4b05e76654
Evgeniy-Khl <graviton.ua@gmail.com>
2025-03-13 06:24:32 +0200

    add dir MDK-ARM

---
+++ MDK-ARM/GRD_41/fonts.d
@@ -0,0 +1,3 @@
+grd_41\fonts.o: ..\Lib\st7796\fonts.c
+grd_41\fonts.o: ..\Lib\st7796\fonts.h
+grd_41\fonts.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ MDK-ARM/GRD_41/startup_stm32f103xb.d
@@ -0,0 +1,1 @@
+grd_41\startup_stm32f103xb.o: startup_stm32f103xb.s
